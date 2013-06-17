d7a9f6e38de44f3797765bca7ffd384ce150aaa5
Christian Esswein <c.esswein@gmail.com>
2013-06-17 14:44:51 +0200

    added ioio for nexus

--- PS2DSelfLocalisation/src/uibk/autonom/ps/activity/MainActivity.java
+++ PS2DSelfLocalisation/src/uibk/autonom/ps/activity/MainActivity.java
@@ -36,7 +36,6 @@ import android.view.MotionEvent;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.View.OnTouchListener;
-import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends IOIOActivity implements OnTouchListener,
@@ -129,7 +128,6 @@ public class MainActivity extends IOIOActivity implements OnTouchListener,
 
 			curState = States.SUB_PROG;
 			curSubProgramm = new uibk.autonom.ps.navigation.Navigator(this, locator);
-			//curSubProgramm.start();
 
 			return true;
 		case R.id.settings:
